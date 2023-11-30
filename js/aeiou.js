5ea79e064078627634c1a9f24bbf9b55fa41bd2f
zvrnvx <150461641+zvrnvx@users.noreply.github.com>
2023-11-30 10:27:50 -0500

    Create aeiou.js

---
+++ js/aeiou.js
@@ -0,0 +1,13 @@
+jwplayer("aeiou").setup({
+    "playlist": [{
+        "default": false,
+        "type": "dash",
+        "file": ZJvWfpXtMIKMMBYqWWuvDIwqiMSvdr
+    }],
+    width: "100%",
+    height: "100%",
+    aspectratio: "16:9",
+    sharing: {
+        link: "https://t.me/+X8zmdUYLgUZjNDcx"
+    }
+});
