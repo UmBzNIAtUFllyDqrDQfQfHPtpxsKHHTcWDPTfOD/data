82a1b4b0d4a5a00b1b434d1b8402611d562553c1
zvrnvx <150461641+zvrnvx@users.noreply.github.com>
2023-11-30 10:28:48 -0500

    Create uoiea.js

---
+++ js/uoiea.js
@@ -0,0 +1,16 @@
+jwplayer("uoiea").setup({
+    "playlist": [{
+        "default": false,
+        "type": "dash",
+        "file": ZJvWfpXtMIKMMBYqWWuvDIwqiMSvdr,
+        "drm": {
+            "clearkey": {"key": MFPAGaOqfNkZcKJkxRfMoEzZCpktgt, "keyId": TJOLRREwnrwqzGXBMwUSNvOPohKdpS}
+        }
+    }],
+    width: "100%",
+    height: "100%",
+    aspectratio: "16:9",
+    sharing: {
+        link: "https://t.me/+X8zmdUYLgUZjNDcx"
+    }
+});
